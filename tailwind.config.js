8086799691916034ef2b2d9184cbc90d7edab429
Shoqan <tataev.shokan@gmail.com>
2023-12-02 15:44:48 +0600

    feature [redesign]

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
